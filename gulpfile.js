ce7084209907ce340788dda2702a7588230d009f
Brant Burnett <bburnett@centeredgesoftware.com>
2018-04-10 14:02:37 -0400

    Add initial unit test framework (#15)

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const plumber = require('gulp-plumber');
 const sourcemaps = require('gulp-sourcemaps');
+const jasmine = require('gulp-jasmine');
+const JasmineConsoleReporter = require('jasmine-console-reporter');
 const del = require('del');
+
+// Ensures that stack traces use sourcemaps to refer to original files
+require('source-map-support').install();
 
 gulp.task('clean', () => {
     // You can use multiple globbing patterns as you would with `gulp.src`
@@ -21,3 +26,24 @@ gulp.task('build', () =>
 gulp.task('watch', ['build'], () => {
     gulp.watch('app/**/*.js', ['build']);
 });
+
+gulp.task('testbuild', () =>
+    gulp.src(['app/**/*.js', 'test/**/*.js'], {base: './'})
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(sourcemaps.write('.', {sourceRoot: '../'}))
+        .pipe(gulp.dest('testbin'))
+);
+
+gulp.task('test', ['testbuild'], () =>
+    gulp.src('testbin/test/**/*.spec.js')
+        .pipe(plumber({
+            errorHandler: () => {},
+        }))
+        .pipe(jasmine({
+            reporter: new JasmineConsoleReporter({
+                colors: true,
+            }),
+        }))
+);
