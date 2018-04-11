d56d1f8cb639b1196fba26d42e6d28d5fd190afd
Brant Burnett <bburnett@centeredgesoftware.com>
2018-04-11 11:06:32 -0400

    Issue #14: Handle pre-5.0 admin API auth
    Motivation
    ----------
    Getting index statuses is not working on pre-5.0 clusters because it
    requires authentication though other operations do not.
    
    Modifications
    -------------
    Use a ClusterManager instead of BucketManager for cluster level admin
    API requests.
    
    Results
    -------
    Admin API calls now succeed on both 4.0 and 5.0 clusters.

--- app/sync.js
+++ app/sync.js
@@ -94,8 +94,6 @@ export class Sync {
             clusterVersion: await this.manager.getClusterVersion(),
         };
 
-        console.log(mutationContext.clusterVersion);
-
         if (mutationContext.clusterVersion.major < 5) {
             // Force all definitions to use manual replica management
             definitions.forEach((def) => {
