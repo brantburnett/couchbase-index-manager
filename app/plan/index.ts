a29cc5b6f4082eaf43afa599b30882cb0d109850
Brant Burnett <bburnett@centeredgesoftware.com>
2021-08-31 12:09:07 +0000

    Finish cleanup of index-manager collection support
    - Now includes drop index support
    - Fixes a couple of bugs
    - Also did a bit of cleanup elsewhere

---
+++ app/plan/index.ts
@@ -0,0 +1,7 @@
+export * from './plan';
+export * from './index-mutation';
+export * from './create-index-mutation';
+export * from './drop-index-mutation';
+export * from './move-index-mutation';
+export * from './resize-index-mutation';
+export * from './update-index-mutation';
