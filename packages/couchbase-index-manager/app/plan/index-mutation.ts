c29792beaaf2c805acbd979aeed860087fd7ce3b
Brant Burnett <bburnett@centeredgesoftware.com>
2021-10-18 14:04:29 +0000

    Support named collections in _default scope
    Motivation
    ------------
    Currently doesn't work correctly with collections in the _default scope
    except for the _default collection.
    
    Modifications
    ---------------
    Check for _default in both scope and collection before assuming that an
    index is in the default collection.
    
    Fixes #93

--- packages/couchbase-index-manager/app/plan/index-mutation.ts
+++ packages/couchbase-index-manager/app/plan/index-mutation.ts
@@ -1,5 +1,5 @@
 import { IndexDefinition } from "../definition";
-import { DEFAULT_SCOPE, IndexManager } from "../index-manager";
+import { DEFAULT_COLLECTION, DEFAULT_SCOPE, IndexManager } from "../index-manager";
 import { Logger } from "../options";
 
 /**
@@ -15,7 +15,7 @@ export abstract class IndexMutation {
      * A display name for the index which includes the scope and collection if non-default.
      */
     get displayName(): string {
-        if (this.scope === DEFAULT_SCOPE) {
+        if (this.scope === DEFAULT_SCOPE && this.collection === DEFAULT_COLLECTION) {
             return this.name;
         } else {
             return `${this.scope}.${this.collection}.${this.name}`;
